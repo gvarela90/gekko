51ff31407091d20b07a2a35111e667d2ad9353c6
Gabriel Aguilar <gavarela90@gmail.com>
2019-11-07 12:21:07 -0600

    -Add env parameters

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -9,18 +9,16 @@ const CONFIG = {
   api: {
     host: '127.0.0.1',
     port: 3000,
-    timeout: 120000 // 2 minutes
+    timeout: 120000, // 2 minutes
   },
   ui: {
     ssl: false,
-    host: 'localhost',
-    port: 3000,
-    path: '/'
+    host: `${process.env.DOMAIN || 'localhost'}`,
+    port: `${process.env.PORT || 3000}`,
+    path: '/',
   },
-  adapter: 'sqlite'
-}
+  adapter: 'sqlite',
+};
 
-if(typeof window === 'undefined')
-  module.exports = CONFIG;
-else
-  window.CONFIG = CONFIG;
+if (typeof window === 'undefined') module.exports = CONFIG;
+else window.CONFIG = CONFIG;
