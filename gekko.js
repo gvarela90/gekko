fcc6933d1d79a81e6a7078d26704cfcab6a456e4
Gabriel Aguilar <gavarela90@gmail.com>
2019-11-07 10:34:52 -0600

    -Add slack plugin -Add neuralnet plugin

--- gekko.js
+++ gekko.js
@@ -35,11 +35,11 @@ console.log(`
 const util = require(__dirname + '/core/util');
 
 console.log('\tGekko v' + util.getVersion());
-console.log('\tI\'m gonna make you rich, Bud Fox.', '\n\n');
+console.log("\tI'm gonna make you rich, Bud Fox.", '\n\n');
 
 const dirs = util.dirs();
 
-if(util.launchUI()) {
+if (util.launchUI()) {
   return require(util.dirs().web + 'server');
 }
 
@@ -47,16 +47,17 @@ const pipeline = require(dirs.core + 'pipeline');
 const config = util.getConfig();
 const mode = util.gekkoMode();
 
-if(
+if (
   config.trader.enabled &&
   !config['I understand that Gekko only automates MY OWN trading strategies']
 )
-  util.die('Do you understand what Gekko will do with your money? Read this first:\n\nhttps://github.com/askmike/gekko/issues/201');
+  util.die(
+    'Do you understand what Gekko will do with your money? Read this first:\n\nhttps://github.com/askmike/gekko/issues/201'
+  );
 
 // > Ever wonder why fund managers can't beat the S&P 500?
 // > 'Cause they're sheep, and sheep get slaughtered.
 pipeline({
   config: config,
-  mode: mode
+  mode: mode,
 });
-
--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -13,12 +13,63 @@ config.debug = true;
 //                       CONFIGURING TRADING ADVICE
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+// Added
+config.MACD = {
+  // EMA weight (α)
+  // the higher the weight, the more smooth (and delayed) the line
+  short: 10,
+  long: 21,
+  signal: 9,
+  // the difference between the EMAs (to act as triggers)
+  thresholds: {
+    down: -0.025,
+    up: 0.025,
+    // How many candle intervals should a trend persist
+    // before we consider it real?
+    persistence: 1,
+  },
+};
+
+// added
 config.tradingAdvisor = {
-}
+  enabled: true,
+  method: 'MACD',
+  candleSize: 60,
+  historySize: 5,
+};
+
+// added
+config.paperTrader = {
+  enabled: true,
+  // report the profit in the currency or the asset?
+  reportInCurrency: true,
+  // start balance, on what the current balance is compared with
+  simulationBalance: {
+    // these are in the unit types configured in the watcher.
+    asset: 1,
+    currency: 100,
+  },
+  // how much fee in % does each trade cost?
+  feeMaker: 0.15,
+  feeTaker: 0.25,
+  feeUsing: 'maker',
+  // how much slippage/spread should Gekko assume per trade?
+  slippage: 0.05,
+};
+
+// added
+config.slack = {
+  enabled: true,
+  token:
+    'xoxp-123608068016-453028982224-826305826487-e15af899be1787f0d6adb55244ced344',
+  sendMessageOnStart: true,
+  muteSoft: true,
+  channel: 'GQAKJTNJ2', // #tradebot
+};
 
 config.candleWriter = {
-  enabled: false
-}
+  enabled: false,
+};
 
 config.backtestResultExporter = {
   enabled: false,
@@ -27,13 +78,13 @@ config.backtestResultExporter = {
     stratUpdates: false,
     roundtrips: true,
     stratCandles: true,
-    trades: true
-  }
-}
+    trades: true,
+  },
+};
 
 config.childToParent = {
   enabled: false,
-}
+};
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING ADAPTER
@@ -47,40 +98,46 @@ config.sqlite = {
   version: 0.1,
   dataDirectory: 'history',
   journalMode: require('../isWindows.js') ? 'PERSIST' : 'WAL',
-  dependencies: [{
-    module: 'sqlite3',
-    version: '3.1.4'
-  }]
-}
+  dependencies: [
+    {
+      module: 'sqlite3',
+      version: '3.1.4',
+    },
+  ],
+};
 
-  // Postgres adapter example config (please note: requires postgres >= 9.5):
+// Postgres adapter example config (please note: requires postgres >= 9.5):
 config.postgresql = {
   path: 'plugins/postgresql',
   version: 0.1,
   connectionString: 'postgres://user:pass@localhost:5432', // if default port
   database: null, // if set, we'll put all tables into a single database.
   schema: 'public',
-  dependencies: [{
-    module: 'pg',
-    version: '7.4.3'
-  }]
-}
+  dependencies: [
+    {
+      module: 'pg',
+      version: '7.4.3',
+    },
+  ],
+};
 
 // Mongodb adapter, requires mongodb >= 3.3 (no version earlier tested)
 config.mongodb = {
   path: 'plugins/mongodb',
   version: 0.1,
   connectionString: 'mongodb://mongodb/gekko', // connection to mongodb server
-  dependencies: [{
-    module: 'mongojs',
-    version: '2.4.0'
-  }]
-}
+  dependencies: [
+    {
+      module: 'mongojs',
+      version: '2.4.0',
+    },
+  ],
+};
 
 config.adviceWriter = {
   enabled: false,
   muteSoft: true,
-}
+};
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING BACKTESTING
@@ -91,14 +148,14 @@ config.adviceWriter = {
 
 config.backtest = {
   daterange: 'scan',
-  batchSize: 50
-}
+  batchSize: 50,
+};
 
 config.importer = {
   daterange: {
     // NOTE: these dates are in UTC
-    from: "2016-06-01 12:00:00"
-  }
-}
+    from: '2016-06-01 12:00:00',
+  },
+};
 
 module.exports = config;
